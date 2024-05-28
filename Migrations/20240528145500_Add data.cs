bcb379f77fc737a84328b7937c3706fdb5e259ce
Amos <asidelnik@gmail.com>
2024-05-28 18:07:40 +0300

    DB - Insert data - Users & InsurancePolicies

---
+++ Migrations/20240528145500_Add data.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sqlink.Migrations
+{
+  /// <inheritdoc />
+  public partial class Adddata : Migration
+  {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('John Doe', 'john.doe@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Jane Doe', 'jane.doe@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Alice Smith', 'alice.smith@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Bob Smith', 'bob.smith@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Charlie Brown', 'charlie.brown@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Daisy Brown', 'daisy.brown@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Eva Johnson', 'eva.johnson@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Frank Johnson', 'frank.johnson@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Grace Davis', 'grace.davis@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Henry Davis', 'henry.davis@example.com')");
+      migrationBuilder.Sql("INSERT INTO Users (Name, Email) VALUES ('Ivy Wilson', 'ivy.wilson@example.com')");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+  }
+}
---
+++ Migrations/20240528150214_Insurance Policies.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sqlink.Migrations
+{
+  /// <inheritdoc />
+  public partial class InsurancePolicies : Migration
+  {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES('123456', 1000, '2024-05-01', '2025-05-01', 1)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('654321', 2000, '2024-05-01', '2025-05-01', 2)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('111222', 3000, '2024-05-01', '2025-05-01', 3)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('222111', 4000, '2024-05-01', '2025-05-01', 4)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('333444', 5000, '2024-05-01', '2025-05-01', 5)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('444333', 6000, '2024-05-01', '2025-05-01', 6)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('555666', 7000, '2024-05-01', '2025-05-01', 7)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('666555', 8000, '2024-05-01', '2025-05-01', 8)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('777888', 9000, '2024-05-01', '2025-05-01', 9)");
+      migrationBuilder.Sql("INSERT INTO InsurancePolicies (PolicyNumber, InsuranceAmount, StartDate, EndDate, UserId) VALUES ('888777', 10000, '2024-05-01', '2025-05-01', 10)");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+  }
+}
